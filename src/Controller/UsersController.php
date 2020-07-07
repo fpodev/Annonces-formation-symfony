2ff10be0089c620194166f4bc182837105979d37
fpodev <fpo@gmx.fr>
2020-07-07 17:22:56 +0200

    Modif index, ajout editprofil + editpass

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -4,9 +4,12 @@ namespace App\Controller;
 
 use App\Entity\Annonces;
 use App\Form\AnnoncesType;
+use App\Form\EditProfileType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 
 class UsersController extends AbstractController
 {
@@ -15,9 +18,7 @@ class UsersController extends AbstractController
      */
     public function index()
     {
-        return $this->render('users/index.html.twig', [
-            'controller_name' => 'UsersController',
-        ]);
+        return $this->render('users/index.html.twig');
     }
 
     /**
@@ -45,5 +46,53 @@ class UsersController extends AbstractController
         return $this->render('users/annonces/ajout.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+    /**
+     * @Route("/users/profil/modifier", name="users_profil_modifier")
+     */
+    public function EditProfile(Request $request)
+    {     
+        $user = $this->getUser();
+        
+        $form = $this->createForm(EditProfileType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('message', 'Profil mis à jour');
+            return $this->redirectToRoute('users');
+        }
+
+        return $this->render('users/editprofile.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/users/passe/modifier", name="users_passe_modifier")
+     */
+    public function EditPass(Request $request, UserPasswordEncoderInterface $passwordencoder)
+    {
+        if($request->isMethod('POST')){
+            $em = $this->getDoctrine()->getManager();
+
+            $user = $this->getUser();
+
+            if($request->request->get('pass') == $request->request->get('pass2')){
+                $user->setPassword($passwordencoder->encodePassword($user, $request->request->get('pass')));
+                $em->flush();
+                $this->addFlash('message', 'Le mot de passe est modifier');
+
+                return $this->redirectToRoute('users');
+            }else{
+                $this->addFlash('erreur', 'Les 2 mots de passe ne sont pas identiques');
+            }
+        }
+        return $this->render('users/editpass.html.twig');
+         
     }
 }
