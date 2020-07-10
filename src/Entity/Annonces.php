178984175d621f5ef1cca005e3adcc3377ced7b8
fpodev <fpo@gmx.fr>
2020-07-10 16:55:05 +0200

    Ajout $images et $favori

--- src/Entity/Annonces.php
+++ src/Entity/Annonces.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\AnnoncesRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
 
@@ -56,6 +58,22 @@ class Annonces
      * @ORM\JoinColumn(nullable=false)
      */
     private $categories;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Images::class, mappedBy="annonces", orphanRemoval=true)
+     */
+    private $images;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Users::class, inversedBy="favori")
+     */
+    private $favori;
+
+    public function __construct()
+    {
+        $this->images = new ArrayCollection();
+        $this->favori = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -128,6 +146,63 @@ class Annonces
     public function setCategories(?Categories $categories): self
     {
         $this->categories = $categories;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Images[]
+     */
+    public function getImages(): Collection
+    {
+        return $this->images;
+    }
+
+    public function addImage(Images $image): self
+    {
+        if (!$this->images->contains($image)) {
+            $this->images[] = $image;
+            $image->setAnnonces($this);
+        }
+
+        return $this;
+    }
+
+    public function removeImage(Images $image): self
+    {
+        if ($this->images->contains($image)) {
+            $this->images->removeElement($image);
+            // set the owning side to null (unless already changed)
+            if ($image->getAnnonces() === $this) {
+                $image->setAnnonces(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Users[]
+     */
+    public function getFavori(): Collection
+    {
+        return $this->favori;
+    }
+
+    public function addFavori(Users $favori): self
+    {
+        if (!$this->favori->contains($favori)) {
+            $this->favori[] = $favori;
+        }
+
+        return $this;
+    }
+
+    public function removeFavori(Users $favori): self
+    {
+        if ($this->favori->contains($favori)) {
+            $this->favori->removeElement($favori);
+        }
 
         return $this;
     }
