adaed13bcd942adc78ea3ed56736972c6afffaaf
fpodev <fpo@gmx.fr>
2020-07-21 16:05:34 +0200

    Ajout formulaire ContactType

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use App\Repository\AnnoncesRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\MailerInterface;
 
 class MainController extends AbstractController
 {
@@ -15,6 +19,39 @@ class MainController extends AbstractController
     {
         return $this->render('main/index.html.twig', [
             'annonces' => $annoncesRepo->findBy(['active' => true], ['created_at' => 'desc']),
+        ]);
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(Request $request, MailerInterface $mailer)
+    {
+        $form = $this->createForm(ContactType::class);
+
+        $contact = $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $email = (new TemplatedEmail())
+                ->from($contact->get('email')->getData())
+                ->to('vous@domaine.fr')
+                ->subject('Contact depuis le site d\'annonces')
+                ->htmlTemplate('emails/contact.html.twig')
+                ->context([
+                    'mail' => $contact->get('email')->getData(),
+                    'sujet' => $contact->get('sujet')->getData(),
+                    'message' => $contact->get('message')->getData()
+
+                ]);
+
+                $mailer->send($email);
+
+                $this->addflash('message', 'email de contact envoyé');
+
+                return $this->redirectToRoute('contact');
+        }
+        return $this->render('main/contact.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
